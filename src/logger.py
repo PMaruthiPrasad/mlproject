f27312d9fbc1503da3ef2b65a8de8bbe31162cd0
origin <https://github.com/PMaruthiPrasad/test.git>
2023-08-11 06:55:08 +0530

    EDA and problem statement

--- src/logger.py
+++ src/logger.py
@@ -5,6 +5,7 @@ import logging
 from exception import CustomException
 import sys
 
+
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
@@ -15,11 +16,3 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,)
 
-if __name__ == "__main__":
-
-    try:
-        a=1/0
-    except Exception as e:
-        
-        logging.info("divide by zero error")
-        raise CustomException(e,sys)
