c2ce6bde3f63891c58bd20e36df929ea37f775ec
origin <https://github.com/PMaruthiPrasad/test.git>
2023-09-08 05:27:59 +0530

    web app issue resolved and working in local machine

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import os
 from src.exception import CustomException
 
 from src.utils import load_object
@@ -10,13 +11,16 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
-            preds=model.prect(data_scaled)
+            preds=model.predict(data_scaled)
             return preds
+        
         except Exception as e:
             raise CustomException(e,sys)
 
@@ -45,3 +49,20 @@ class CustomData:
         self.reading_score = reading_score
 
         self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
